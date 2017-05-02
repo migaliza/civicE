ed631602265a82d999752b0b59db04682a2233bc
migaliza <migaliza.lyza@gmail.com>
2017-05-02 08:40:00 +0000

    made changes to config/database by including host and port

--- config/database.php
+++ config/database.php
@@ -34,8 +34,8 @@ return [
     'connections' => [
         'mongodb' => [
             'driver'   => 'mongodb',
-            'host'     => env('DB_HOST'),
-            'port'     => env('DB_PORT'),
+            'host'     => env('DB_HOST','ds123351.mlab.com'),
+            'port'     => env('DB_PORT','23351'),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
