e46974db51c1c2d45f8bcdfead4320769452d9fd
migaliza <migaliza.lyza@gmail.com>
2017-05-18 19:05:20 +0000

    ui for master

---
+++ resources/views/API/civic.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Civic </title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/beta_css/bootstrap.min.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/customize.min.css') }}">
+  
+
+    <!-- MetisMenu CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/beta_css/metisMenu.min.css') }}">
+
+    <!-- Timeline CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/beta_css/timeline.css') }}">
+
+    <!-- Custom CSS -->
+     <link rel="stylesheet" type="text/css" href="{{ asset('css/beta_css/startmin.css') }}">
+
+    <!-- Morris Charts CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/beta_css/morris.css') }}">
+
+    <!-- Custom Fonts -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/beta_css/font-awesome.min.css') }}">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div id="wrapper">
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Civic Engagement Portal</a>
+        </div>
+
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+        <!-- Top Navigation: Left Menu -->
+        
+      
+    @if (Route::has('login'))
+    <div>
+        @if (Auth::check())
+            <ul class="nav navbar-nav navbar-left navbar-top-links">
+                <li><a href="{{ url('/api') }}"><i class="fa fa-home fa-fw"></i>Home</a></li>
+            </ul>
+        @else
+        <ul class="nav navbar-right navbar-top-links">
+            <li class="dropdown navbar-inverse">
+                <a href="#">
+                    <i class="fa fa-bell fa-fw"></i> Login<b class="caret"></b>
+                </a>   
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-user fa-fw"></i> Register <b class="caret"></b>
+                </a>
+            </li>
+        </ul>
+        @endif
+    </div>
+    @endif
+      
+
+    <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse">
+
+                <ul class="nav" id="side-menu">
+                    
+                    <li>
+                        <a href="#" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('apiProject') }}"><i class="fa fa-sitemap fa-fw"></i> Project</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('apiProject') }}"><i class="fa fa-sitemap fa-fw"></i> Project Tier</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('apiProject') }}"><i class="fa fa-sitemap fa-fw"></i> Milestone</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('apiProject') }}"><i class="fa fa-sitemap fa-fw"></i> Grand Info</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('apiProject') }}"><i class="fa fa-sitemap fa-fw"></i> Target Population</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('apiProject') }}"><i class="fa fa-sitemap fa-fw"></i> Volunteers </a>
+                    </li>
+                    <!--<li>
+                        <a href="{{ route('apiProject') }}"><i class="fa fa-sitemap fa-fw"></i> Project <span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="">View Projects</a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="{{ route('apiProject') }}">View all</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">View By Tier</a>
+                                    </li>
+
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">Project Components <span class="fa arrow"></span></a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="#">Milestone</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Events</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Grant info</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Upcoming</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Milestone</a>
+                                    </li>
+
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="#">Active projects <span class="fa arrow"></span></a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="#">Milestone</a>
+                                    </li>
+
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="#">In active Projects<span class="fa arrow"></span></a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="#">Milestone</a>
+                                    </li>   
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>-->
+                </ul>
+
+            </div>
+        </div>
+</nav>
+
+ <div id="page-wrapper">
+        <div class="container-fluid">
+            <div id="container">
+                @yield('content')
+            </div>
+        </div>
+</div>  
+
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- jQuery -->
+<script src="{{ asset('js/beta_js/jquery.min.js') }}"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="{{ asset('js/beta_js/bootstrap.min.js') }}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+<script src="{{ asset('js/beta_js/bootstrap.min.js') }}"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+<script src="{{ asset('js/beta_js/metisMenu.min.js') }}"></script>
+
+    <!-- Custom Theme JavaScript -->
+<script src="{{ asset('js/beta_js/startmin.js') }}"></script>
+
+</body>
+</html>
---
+++ resources/views/API/project.blade.php
@@ -0,0 +1,88 @@
+@extends('API.civic')
+
+@section('title', 'API')
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Available Projects</h1>
+    </div>
+</div>
+<div>
+    <p>Displays all the project at Ashesi University College. Funded by Ford Foundation and any other foundation</p>
+
+    <h4>Method:  <b>Get</b></h4>
+    <div></div>
+    <h6>Url: <a href="http://ashesicivicengagement-dev.herokuapp.com/civicdoc/projects" >http://ashesicivicengagement-dev.herokuapp.com/civicdoc/projects </a> </h6>
+
+    <div></div>
+    <h6><b>Returns: </b> </h6>
+    <code>[<br> 
+    <samp style="color:#ff0000">projects: </samp><br>[
+{<br>
+<p style="margin-left: 40px"><samp style="color:black">"_id":</samp> "590ccb97b4d8d200713be843",<br></p>
+<p style="margin-left: 40px"><samp style="color:black">"project_namee":</samp> "Future of Africa - Midunu",<br></p>
+<p style="margin-left: 40px"><samp style="color:black">"tier":</samp> <i style="color:blue">"1" </i>,<br></p>
+<p style="margin-left: 40px"><samp style="color:black">"location_name":</samp> <i style="color:blue">"0.0747"</i>,<br></p>
+<p style="margin-left: 40px"><samp style="color:black">"location_latitude":</samp> <i style="color:blue">"5.8143"</i>,<br></p>
+<p style="margin-left: 40px"><samp  style="color:black">"brief_description":</samp> "Mi Dunu is a feeding program for street kids under Future of Africa initiative. It aims at creating a friendly environment where street children feel comfortable to share their experiences with trained volunteers.",<br></p>
+<p style="margin-left: 40px"><samp style="color:black">"commencement_date":</samp> "2016-05-01",<br></p>
+<p style="margin-left: 40px"><samp style="color:black">"completion_date":</samp> "2017-09-30",<br></p>
+<p style="margin-left: 40px"><samp  style="color:black">"primary_activity":</samp> "Weekly nourishment and game time where volunteers attempt to build relatioship with street children which they intend to harness to provide the children with skills, opportunity and resources",<br></p>
+<p style="margin-left: 40px"><samp  style="color:black">"partnerships":</samp> "Helen O'Grady Drama Academy",<br></p>
+<p style="margin-left: 40px"><samp  style="color:black">"updated_at":</samp> "2017-05-05 18:59:35",<br></p>
+<p style="margin-left: 40px"><samp  style="color:black">"created_at":</samp> "2017-05-05 18:59:35",<br></p>
+<p style="margin-left: 40px"><samp cstyle="color:black"><b>"Impact_sectors":</b></samp> [<br></p>
+<p style="margin-left: 40px">{<br></p>
+<p style="margin-left: 80px"><samp style="color:black">"sector_name":</samp> "Welfare"<br></p>
+<p style="margin-left: 40px">},<br></p>
+<p style="margin-left: 40px">{<br></p>
+<p style="margin-left: 80px"><samp style="color:black">"sector_name":</samp> "Mentorship"<br></p>
+<p style="margin-left: 40px">}<br></p>
+],<br>
+<p style="margin-left: 40px"><samp  style="color:black"><b>"Grand_info":</b></samp> [<br></p>
+<p style="margin-left: 40px">{<br></p>
+<p style="margin-left: 80px"><samp  style="color:black">"grant_name":</samp> "Ford Foundation",<br></p>
+<p style="margin-left: 80px"><samp style="color:black">"grant_amount":</samp> <i style="color:blue">"4800"</i>,<br></p>
+<p style="margin-left: 80px"><samp  style="color:black">"funding_cycle":</samp> "Window 2"<br></p>
+<p style="margin-left: 40px">}<br></p>
+],<br>
+<p style="margin-left: 40px"><samp  style="color:black"><b>"Population_Track":</b></samp> [<br></p>
+<p style="margin-left: 40px">{<br></p>
+<p style="margin-left: 80px"><samp  style="color:black">"target_baseline": </samp> <i style="color:blue">"1"</i>,<br></p>
+<p style="margin-left: 80px"><samp  style="color:black">"recent":</samp> <i style="color:blue"> "51"</i><br></p>
+<p style="margin-left: 40px">}<br></p>
+],<br>
+<p style="margin-left: 40px"><samp  style="color:black"><b>"Volunteer_Track":</b></samp> [<br></p>
+<p style="margin-left: 40px">{<br></p>
+<p style="margin-left: 80px"><samp  style="color:black">"target_baseline":</samp> <i style="color:blue"> "45"</i>,<br></p>
+<p style="margin-left: 80px"><samp style="color:black">"recent":</samp> <i style="color:blue">"52"</i><br></p>
+<p style="margin-left: 40px">}<br></p>
+],<br>
+<p style="margin-left: 40px"><samp  style="color:black"><b>"milestones":</b></samp> [<br></p>
+<p style="margin-left: 40px">{<br></p>
+<p style="margin-left: 80px"><samp style="color:black">"milestone_description":</samp> "Midunu has succesfully launched the Relationship building phase of its 4-step approach to allievating the conditions of street children"<br></p>
+<p style="margin-left: 40px">},<br></p>
+<p style="margin-left: 40px">{<br></p>
+<p style="margin-left: 80px"><samp  style="color:black">"milestone_description":</samp> "Midunu has established a partnership with Helen O'Grady Drama Academy in a bid to kick-off the skills building and opportunity creation phase of its 4-step approach. This will culminate in a drama production that will cast children under the Initiative."<br></p>
+<p style="margin-left: 40px">}<br></p>
+],<br>
+<p style="margin-left: 40px"><samp  style="color:black"><b>"Upcoming":</b></samp> [<br></p>
+{<br>
+<p style="margin-left: 80px"><samp style="color:black">"upcoming":</samp> "Drama Production in partnership with Helen O'Grady Drama Academy"<br></p>
+<p style="margin-left: 40px">},<br></p>
+{<br>
+<p style="margin-left: 40px"><samp style="color:black">"upcoming":</samp> "Explore options for eanrollment or re-enrollment of street children"<br></p>
+<p style="margin-left: 40px">},<br></p>
+<p style="margin-left: 40px">{<br></p>
+<p style="margin-left: 80px"><samp style="color:black">"upcoming":</samp> "Resolve issues relating to biographical data of street children"<br></p>
+<p style="margin-left: 40px">}<br></p>
+]<br>
+},<br>
+</code>
+
+
+</div>
+
+@endsection
