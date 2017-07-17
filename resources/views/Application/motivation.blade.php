72d375ac23cc42974b8d160703afed9363b40ac2
migaliza <migaliza.lyza@gmail.com>
2017-07-17 21:00:43 +0000

    project lead mpotivation

--- resources/views/Application/motivation.blade.php
+++ resources/views/Application/motivation.blade.php
@@ -13,36 +13,109 @@
 
 
 		<!-- Text input-->
-		<div class="form-group">
-			<label class="col-md-4 control-label" for="name">Project lead Full Name</label>  
-			<div class="col-md-4">
-				<input id="name" name="name" type="text" placeholder="" class="form-control input-md" required="">
+		<div>
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="name1">Project lead Full Name</label>  
+				<div class="col-md-4">
+					<input id="name1" name="name1" type="text" placeholder="" class="form-control input-md" required="">
 
+				</div>
+			</div>
+
+			<!-- Textarea -->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="motivation1">Motivation</label>
+				<div class="col-md-4">                     
+					<textarea class="form-control" id="motivation1" name="motivation1"></textarea>
+				</div>
+			</div>
+
+			<!-- Text input-->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="cv1">Upload CV</label>  
+				<div class="col-md-4">
+					<input id="cv1" name="cv1" type="file" placeholder="" class="form-control input-md" required="">
+					<span class="help-block">
+						<ul>
+							<li>Save it as a PDF</li>
+							<li>Name the file in the following format: "Full Name CV." ex. Bela Shah CV *</li>
+						</ul>
+					</span>
+
+				</div>
 			</div>
 		</div>
 
-		<!-- Textarea -->
-		<div class="form-group">
-			<label class="col-md-4 control-label" for="motivation">Motivation</label>
-			<div class="col-md-4">                     
-				<textarea class="form-control" id="motivation" name="motivation"></textarea>
+		<div class="h-divider"></div>
+		
+		<div>	
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="name2">Project lead Full Name</label>  
+				<div class="col-md-4">
+					<input id="name2" name="name2" type="text" placeholder="" class="form-control input-md" >
+
+				</div>
+			</div>
+
+			<!-- Textarea -->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="motivation2">Motivation</label>
+				<div class="col-md-4">                     
+					<textarea class="form-control" id="motivation2" name="motivation2"></textarea>
+				</div>
+			</div>
+
+			<!-- Text input-->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="cv2">Upload CV</label>  
+				<div class="col-md-4">
+					<input id="cv2" name="cv2" type="file" placeholder="" class="form-control input-md" >
+					<span class="help-block">
+						<ul>
+							<li>Save it as a PDF</li>
+							<li>Name the file in the following format: "Full Name CV." ex. Bela Shah CV *</li>
+						</ul>
+					</span>
+
+				</div>
 			</div>
 		</div>
 
-		<!-- Text input-->
-		<div class="form-group">
-			<label class="col-md-4 control-label" for="cv">Upload CV</label>  
-			<div class="col-md-4">
-				<input id="cv" name="cv" type="file" placeholder="" class="form-control input-md" required="">
-				<span class="help-block">
-					<ul>
-						<li>Save it as a PDF</li>
-						<li>Name the file in the following format: "Full Name CV." ex. Bela Shah CV *</li>
-					</ul>
-				</span>
+		<div class="h-divider"></div>
 
+		<div>
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="name3">Project lead Full Name</label>  
+				<div class="col-md-4">
+					<input id="name3" name="name3" type="text" placeholder="" class="form-control input-md">
+
+				</div>
+			</div>
+
+			<!-- Textarea -->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="motivation3">Motivation</label>
+				<div class="col-md-4">                     
+					<textarea class="form-control" id="motivation3" name="motivation3"></textarea>
+				</div>
+			</div>
+
+			<!-- Text input-->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="cv3">Upload CV</label>  
+				<div class="col-md-4">
+					<input id="cv3" name="cv3" type="file" placeholder="" class="form-control input-md" >
+					<span class="help-block">
+						<ul>
+							<li>Save it as a PDF</li>
+							<li>Name the file in the following format: "Full Name CV." ex. Bela Shah CV *</li>
+						</ul>
+					</span>
+
+				</div>
 			</div>
 		</div>
+
 		
 		<div class="form-group text-center " id="btnGroup" >
 			<div role="group" class="col-md-4  " >
