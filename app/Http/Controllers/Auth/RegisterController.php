fd59075c7feeb443b15c5cd81277001a26b9fa9e
migaliza <migaliza.lyza@gmail.com>
2017-07-17 12:35:21 +0000

    redirect to login after regoistration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin';
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
