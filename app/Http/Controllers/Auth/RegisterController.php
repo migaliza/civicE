eba6f612ed1b02b125574cd962614e3689f68959
migaliza <migaliza.lyza@gmail.com>
2017-07-17 13:03:46 +0000

    admin dashboard

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -88,6 +88,6 @@ class RegisterController extends Controller
             ]);
 
     }
-    
+
 }
 }
--- resources/views/Phase2/index.blade.php
+++ resources/views/Phase2/index.blade.php
@@ -1,24 +1,26 @@
+@if( Auth::user()->name == 'admin')
 @extends('Phase2.api_layout')
 
 @section('title', 'ADMINISTRATOR')
 
 @section('content')
 
-    <!-- Page Content -->
-    <div id="page-wrapper">
-        <div class="container-fluid">
-        
-           <div class="row">
-               <div class="col-lg-12">
-                   <h1 class="page-header">Available Projects</h1>
-               </div>
-           </div>
-        
-            <div >@yield('child-content')</div>
-        
-            <!-- Your content goes here ...-->
-        
-        </div>
-    </div>
+<!-- Page Content -->
+<div id="page-wrapper">
+  <div class="container-fluid">
+    
+   <div class="row">
+     <div class="col-lg-12">
+       <h1 class="page-header">Available Projects</h1>
+     </div>
+   </div>
+   
+   <div >@yield('child-content')</div>
+   
+   <!-- Your content goes here ...-->
+   
+ </div>
+</div>
 
 @endsection
+@endif
