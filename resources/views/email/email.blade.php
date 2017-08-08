0af5698894db981416d153b32247d73122c12084
migaliza <migaliza.lyza@gmail.com>
2017-08-08 17:55:05 +0000

    email verification

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -1,2 +1,2 @@
-<h5>Click the Link to Verify your Email</h5>
+<h4>Click the Link to Verify your Email</h4>
 Click the following link to verify your email {{ url('https://ashesicivicengagement-dev.herokuapp.com/verifyemail/'.rawurlencode($email_token))}}
