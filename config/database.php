9a8ec645964a6a81fd0f4b155e0003837354294d
migaliza <migaliza.lyza@gmail.com>
2017-08-08 12:59:17 +0000

    made changes ticonfig/database to fix register error

--- config/database.php
+++ config/database.php
@@ -34,16 +34,16 @@ return [
     'connections' => [
     'mongodb' => [
     'driver'   => 'mongodb',
-    'host'     => env('DB_HOST','cecapp'),
-    'port'     => env('DB_PORT','27017'),
+    'host'     => env('DB_HOST','ds123351.mlab.com'),
+    'port'     => env('DB_PORT','23351'),
     'database' => env('DB_DATABASE'),
     'username' => env('DB_USERNAME'),
     'password' => env('DB_PASSWORD'),
     'options'  => array(
-             // 'database' => 'heroku_2gz3f54l', // sets the authentication database required by mongo 3
-        'database' => 'cecapp'
-        
-        )
+              'database' => 'heroku_2gz3f54l', // sets the authentication database required by mongo 3
+        //'database' => 'cecapp'
+              
+              )
     ],
 
     'sqlite' => [
