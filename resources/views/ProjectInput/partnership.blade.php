7e39c63797b830e4d1c0c24493de4c2853a8728b
migaliza <migaliza.lyza@gmail.com>
2017-08-11 18:59:30 +0000

    partnership

--- resources/views/ProjectInput/partnership.blade.php
+++ resources/views/ProjectInput/partnership.blade.php
@@ -12,39 +12,40 @@
       </div>
       <form class="form-horizontal" method="POST" action="civicdoc/project/partnership/new">
         <fieldset>
-         
+         {{ csrf_field() }}
 
-          <div class="form-group">
-            <label class="col-md-4 control-label" for="pId"></label>  
-            <div class="col-md-4">
-              <input id="pId" name="pId" type="hidden" value={{ $projectId }} class="form-control input-md">
-            </div>
+
+         <div class="form-group">
+          <label class="col-md-4 control-label" for="pId"></label>  
+          <div class="col-md-4">
+            <input id="pId" name="pId" type="hidden" value={{ $projectId }} class="form-control input-md">
           </div>
+        </div>
 
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-md-4 control-label" for="partnership">Partnership</label>  
-            <div class="col-md-4">
-              <input id="partnership" name="partnership" type="text" placeholder="" class="form-control input-md" required="">
-              <span class="help-block">Name of partner</span>  
-            </div>
+        <!-- Text input-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="partnership">Partnership</label>  
+          <div class="col-md-4">
+            <input id="partnership" name="partnership" type="text" placeholder="" class="form-control input-md" required="">
+            <span class="help-block">Name of partner</span>  
           </div>
+        </div>
 
-          <!-- Textarea -->
-          <div class="form-group">
-            <label class="col-md-4 control-label" for="description">Description</label>
-            <div class="col-md-4">                     
-              <textarea class="form-control" id="description" name="description"></textarea>
-            </div>
+        <!-- Textarea -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="description">Description</label>
+          <div class="col-md-4">                     
+            <textarea class="form-control" id="description" name="description"></textarea>
           </div>
+        </div>
 
-          <!-- Button -->
-          <div class="form-group">
-            <label class="col-md-4 control-label" for="submit"></label>
-            <div class="col-md-4">
-              <button id="submit" name="submit" class="btn btn-primary">ADD</button>
-            </div>
+        <!-- Button -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="submit"></label>
+          <div class="col-md-4">
+            <button id="submit" name="submit" class="btn btn-primary">ADD</button>
           </div>
+        </div>
 
-        </fieldset>
-      </form>
+      </fieldset>
+    </form>
