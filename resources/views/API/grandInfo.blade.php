9125e2ecb73205527824835b85fdf037b13617f2
migaliza <migaliza.lyza@gmail.com>
2017-08-13 08:23:15 +0000

    funding info

--- resources/views/API/grandInfo.blade.php
+++ resources/views/API/grandInfo.blade.php
@@ -5,44 +5,45 @@
 @section('content')
 
 <div class="row">
-    <div class="col-lg-12">
-        <h1 class="page-header">Grand Info</h1>
-    </div>
+  <div class="col-lg-12">
+    <h1 class="page-header">Grand Info</h1>
+  </div>
 </div>
 <div>
-    <p>Displays grand info for the project.</p>
-    
+  <p>Displays grand info for the project.</p>
+  
 
-    <div></div>
+  <div></div>
 
-    <h4>Method:  <b>Get</b></h4>
-    <div></div>
-    <h5>Url: <a href="https://ashesicivicengagement-dev.herokuapp.com/civicdoc/projects/funding" target="_blank"> https://ashesicivicengagement-dev.herokuapp.com/civicdoc/projects/funding?<b>id=kjsfjks</b></a> </h5>
+  <h4>Method:  <b>Get</b></h4>
+  <div></div>
+  <h5>Url: <a href="https://ashesicivicengagement-dev.herokuapp.com/civicdoc/projects/funding?pId=" target="_blank"> https://ashesicivicengagement-dev.herokuapp.com/civicdoc/projects/funding?pId=<b>598cb39fec432900711efc22</b></a> </h5>
 
-    <div></div>
-    <h5>Parameters: </h5>
-    <ul>
-        <li>project id i.e <i>79827kjdhj</i></li>
-    </ul>
-    
-    
-    <div></div>
-    <h6><b>Returns: </b> </h6>
-    <code>
-       {<br>
-       {<br>
-       <samp style="margin-left:40px;color:#ff0000">"Funding": [</samp><br>
-       <samp style="margin-left: 80px">{</samp><br>
-       <samp style="margin-left: 120px;color:black">"FundingInfo": [</samp><br>
-       <samp style="margin-left: 160px">{</samp><br>
-       <samp style="margin-left: 200px;color:black">"grant_name":</samp> "Ford Foundation",<br>
-       <samp style="margin-left: 200px;color:black">"grant_amount":</samp> "3800",<br>
-       <samp style="margin-left: 200px;color:black">"funding_cycle":</samp> "Window 1"<br>
-       <samp style="margin-left: 160px">}</samp><br>
-       <samp style="margin-left: 120px;color:black">]</samp><br>
-       <samp style="margin-left: 80px">}</samp><br>
-       <samp style="margin-left:40px;color:#ff0000">]</samp><br>
-   }<br>
+  <div></div>
+  <h5>Parameters: </h5>
+  <ul>
+    <li>pId (project Id) i.e <i>598cb39fec432900711efc22</i></li>
+  </ul>
+  
+  
+  <div></div>
+  <h6><b>Returns: </b> </h6>
+  <code>
+   {<br>
+   <samp style="margin-left:40px;color:green">"success": </samp><samp style="color: blue">true</samp>,<br>
+   <samp style="margin-left:40px;color:green">"code": </samp><samp style="color: blue">0</samp>,<br>
+   <samp style="margin-left:40px;color:green">"local": </samp><samp style="color: green">"en"</samp>,<br>
+   <samp style="margin-left:40px;color:green">"message": </samp><samp style="color: green">"OK"</samp>,<br>
+   <samp style="margin-left:40px;color:green">"data": </samp>[<br>
+   <samp style="margin-left: 80px">{</samp><br>
+   <samp style="margin-left: 120px;color:black">"fPartner": "Ford Foundation"</samp><br>
+   <samp style="margin-left: 120px;color:black">"fAmount": </samp><samp style="color: blue">3800</samp>,<br>
+   <samp style="margin-left: 120px;color:black">"fObjective": "This will be updated as soon as the right data is found"</samp><br>
+   <samp style="margin-left: 120px;color:black">"fDescription": "This will be updated as soon as the right data is found"</samp><br>
+   <samp style="margin-left: 80px">}</samp><br>
+   <samp style="margin-left:40px">]</samp><br>
+
+ }<br>
 </code>
 
 </div>
