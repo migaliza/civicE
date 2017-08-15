ce0e3529d6d48c0c7486398fe277118c02d9339d
migaliza <migaliza.lyza@gmail.com>
2017-08-15 17:50:48 +0000

    project id

--- app/Http/Controllers/projectRetriveController.php
+++ app/Http/Controllers/projectRetriveController.php
@@ -221,7 +221,7 @@ class projectRetriveController extends Controller
     public function projectInformation(Request $request)
     {
         $pId = $request->input('pId');
-        dd($pId);
+        dd("project Id ".$pId);
         if(!empty($pId)){
             $project = projects::where('_id','=',$pId)->get();
             dd($project);
@@ -268,8 +268,8 @@ class projectRetriveController extends Controller
     public function trackPopulation(Request $request){
         $projectId = $request->get('id');
         if(!is_null($projectId)){
-           $population = projects::where('_id','=',$projectId)->get(['Population_Track']);
-           if(!is_null($population)){
+         $population = projects::where('_id','=',$projectId)->get(['Population_Track']);
+         if(!is_null($population)){
             return ResponseBuilder::success($population);
         }
     }
