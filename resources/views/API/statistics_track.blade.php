d8596f2f16a4d6b97b9a6dee18985a546519fd9b
migaliza <migaliza.lyza@gmail.com>
2017-08-16 19:01:33 +0000

    statistics api doc

--- resources/views/API/statistics_track.blade.php
+++ resources/views/API/statistics_track.blade.php
@@ -23,28 +23,28 @@
     <h5>Parameters: </h5>
     <ul>
         <li>pId i.e <b><i>79827kjdhj</i></i></li>
-        <li>track i.e <b>id=population <i style="color:green">to display the target population cumulative values</i></i><i style="color: blue">volunteer to display the volunteer track over time</i></li>
+        <li>track i.e <b>population</b> <i style="color:green">to display the target population cumulative values</i>or <b>volunteer </b></i><i style="color: blue">to display the volunteer track over time</i></li>
     </ul>
 
 
     <div></div>
     <h6><b>Returns: </b> </h6>
     <code>
-     {<br>
-     <samp style="margin-left:40px;color:green">"success":</samp><samp style="color: blue">true,</samp><br>
-     <samp style="margin-left:40px;color:green">"code":</samp><samp style="color: blue">0,</samp><br>
-     <samp style="margin-left:40px;color:green">"locale": "en"</samp><br>
-     <samp style="margin-left:40px;color:green">"message": "OK"</samp><br>
-     <samp style="margin-left:40px;color:green">"data": [</samp><br>
-     <samp style="margin-left: 80px">{</samp><br>
-     <samp style="margin-left: 120px;color:black">"cumulative":</samp><samp style="color: blue">4,</samp><br>
-     <samp style="margin-left: 120px;color:black">"date": {</samp><br>
-     <samp style="margin-left: 160px">"timezone_type": </samp><samp style="color: blue">3</samp><br>
-     <samp style="margin-left: 160px">"timezone": "UTC"</samp><br>
-     <samp style="margin-left: 120px;color:black">}</samp><br>
-     <samp style="margin-left: 80px">}</samp><br>
-     <samp style="margin-left:40px;color:green">]</samp><br>
- }<br>
+       {<br>
+       <samp style="margin-left:40px;color:green">"success":</samp><samp style="color: blue">true,</samp><br>
+       <samp style="margin-left:40px;color:green">"code":</samp><samp style="color: blue">0,</samp><br>
+       <samp style="margin-left:40px;color:green">"locale": "en"</samp><br>
+       <samp style="margin-left:40px;color:green">"message": "OK"</samp><br>
+       <samp style="margin-left:40px;color:green">"data": [</samp><br>
+       <samp style="margin-left: 80px">{</samp><br>
+       <samp style="margin-left: 120px;color:black">"cumulative":</samp><samp style="color: blue">4,</samp><br>
+       <samp style="margin-left: 120px;color:black">"date": {</samp><br>
+       <samp style="margin-left: 160px">"timezone_type": </samp><samp style="color: blue">3</samp><br>
+       <samp style="margin-left: 160px">"timezone": "UTC"</samp><br>
+       <samp style="margin-left: 120px;color:black">}</samp><br>
+       <samp style="margin-left: 80px">}</samp><br>
+       <samp style="margin-left:40px;color:green">]</samp><br>
+   }<br>
 </code>
 
 </div>
