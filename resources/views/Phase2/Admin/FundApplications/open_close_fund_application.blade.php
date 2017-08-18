28a826bcd16bcfc38b9e57dad77bc75b8f5ee3f7
migaliza <migaliza.lyza@gmail.com>
2017-08-18 18:17:41 +0000

    removing the horizontal bar from the open_close_application view

--- resources/views/Phase2/Admin/FundApplications/open_close_fund_application.blade.php
+++ resources/views/Phase2/Admin/FundApplications/open_close_fund_application.blade.php
@@ -6,47 +6,40 @@
 
 <!-- Page Content -->
 <div id="page-wrapper">
-	<div class="container-fluid">
+	<div class="container-fluid" style="margin-top: 100px; margin-right: 200px">     
+		<form class="form-horizontal">
+			<fieldset>
 
-		<div class="table-responsive" style="margin-top: 100px; margin-right: 200px">      
-			<form class="form-horizontal">
-				<fieldset>
+				<!-- Form Name -->
+				<legend>Open fund application</legend>
 
-					<!-- Form Name -->
-					<legend>Open fund application</legend>
-
-					<!-- Multiple Radios (inline) -->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="open">Action on Application Window</label>
-						<div class="col-md-4"> 
-							<label class="radio-inline" for="open-0">
-								<input type="radio" name="open" id="open-0" value="open" checked="checked">
-								Open
-							</label> 
-							<label class="radio-inline" for="open-1">
-								<input type="radio" name="open" id="open-1" value="close">
-								Close
-							</label>
-						</div>
+				<!-- Multiple Radios (inline) -->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="open">Action on Application Window</label>
+					<div class="col-md-4"> 
+						<label class="radio-inline" for="open-0">
+							<input type="radio" name="open" id="open-0" value="open" >
+							Open
+						</label> 
+						<label class="radio-inline" for="open-1">
+							<input type="radio" name="open" id="open-1" value="close">
+							Close
+						</label>
 					</div>
+				</div>
 
-					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="window">Specify Window</label>  
-						<div class="col-md-4">
-							<input id="window" name="window" type="text" placeholder="" class="form-control input-md">
-							<span class="help-block">specify fund application window i.e window 1 </span>  
-						</div>
+				<!-- Text input-->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="window">Specify Window</label>  
+					<div class="col-md-4">
+						<input id="window" name="window" type="text" placeholder="" class="form-control input-md">
+						<span class="help-block">specify fund application window i.e window 1 </span>  
 					</div>
+				</div>
 
-				</fieldset>
-			</form>    
-			
-		</div>
+			</fieldset>
+		</form>    
 	</div>
-
-
-
 </div>
 </div>
 
