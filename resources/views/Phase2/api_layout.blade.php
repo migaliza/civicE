30589615a9da1eb3888c5c64d838b093f976938f
migaliza <migaliza.lyza@gmail.com>
2017-08-18 19:27:30 +0000

    removed the javascript code

--- resources/views/Phase2/api_layout.blade.php
+++ resources/views/Phase2/api_layout.blade.php
@@ -51,14 +51,11 @@
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
             ]) !!};
-        $(document).ready(function(){
-            $('#datetimepicker1').datetimepicker()
-        });
-    </script>
+        </script>
 
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
