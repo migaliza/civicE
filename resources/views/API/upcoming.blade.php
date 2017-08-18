d0b6680ee8bd8157503c4f7a47b0bdeb64945a22
migaliza <migaliza.lyza@gmail.com>
2017-08-18 16:52:25 +0000

    display api doc page for upcoming

--- resources/views/API/upcoming.blade.php
+++ resources/views/API/upcoming.blade.php
@@ -6,7 +6,7 @@
 
 <div class="row">
     <div class="col-lg-12">
-        <h1 class="page-header">Milestone per project</h1>
+        <h1 class="page-header">Upcoming per project</h1>
     </div>
 </div>
 <div>
